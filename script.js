b3c247069aa5148b5d4501a2c01de10d447bd912
Banzak-Uninter <leosantanabr@gmail.com>
2021-09-28 14:56:48 -0300

    funções para sortear as cores

---
+++ script.js
@@ -0,0 +1,19 @@
+let order = [];
+let clickedOrder = []
+let score = 0;
+
+// 0 - Verde
+// 1 - Vermelho
+// 2 - Amarelo
+// 3 - Azul
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const green = document.querySelector('.green');
+const yellow = document.querySelector('.yellow');
+
+let shufffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+}
