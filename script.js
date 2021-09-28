2be3aa066b669afcd43265fb0b67d2d21e444493
Banzak-Uninter <leosantanabr@gmail.com>
2021-09-28 15:18:19 -0300

    função para o click do usuário

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ const red = document.querySelector('.red');
 const green = document.querySelector('.green');
 const yellow = document.querySelector('.yellow');
 
+
+// Criando ordem aleatória de cores
 let shufffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder;
@@ -23,12 +25,44 @@ let shufffleOrder = () => {
     }
 }
 
+
+// Acende a próxima cor
 let lightColor = (element, number) => {
     number = number * ;
     setTimeout(() => {
         element.classList.add('selected');
     }, number - 250);
     setTimeout(() => {
-        element.classList.remove('.selected');
+        element.classList.remove('selected');
     }, )
 }
+
+
+// Checa se os botões clicados são os mesmos gerada no jogo
+let checkOreder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            lose();
+            break;
+        }
+    }
+
+    if(clickedOrder.length == order.length) {
+        alert(`Pontuação ${score}\nVocê acertou!\nIniciando próximo nível`)
+        nextLevel();
+    }
+}
+
+// Função para o click do usuário
+
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected')
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+    });
+
+    checkOrder();
+}
+
