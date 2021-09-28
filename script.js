627500d5a7aecd504cddcff1dfdc8165415e076d
Banzak-Uninter <leosantanabr@gmail.com>
2021-09-28 15:06:52 -0300

    função para acender os botões

--- script.js
+++ script.js
@@ -16,4 +16,19 @@ let shufffleOrder = () => {
     let colorOrder = Math.floor(Math.random() * 4);
     order[order.length] = colorOrder;
     clickedOrder = [];
+
+    for( let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number{i} + 1);
+    }
+}
+
+let lightColor = (element, number) => {
+    number = number * ;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('.selected');
+    }, )
 }
